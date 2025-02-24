2de78dc8b268b1b92a9bd35e9eb236e24c96f484
DataByte13 <emailaddress@yahoo.com>
2025-02-24 15:06:01 +0330

    rewriting url manager

--- include/UrlManager.h
+++ include/UrlManager.h
@@ -1,16 +1,63 @@
 #ifndef URL_MANAGER_H
 #define URL_MANAGER_H
-
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/json.hpp>
+#include <chrono>
+#include <iostream>
+#include <mongocxx/client.hpp>
+#include <mongocxx/exception/exception.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <regex>
 #include <string>
-#include "FileOperations.h"
+#include <thread>
+#include <vector>
+struct Result_read {
+  bool status;
+  std::string message = "";
+};
 class UrlManager {
 public:
-    UrlManager(FileOperations& obj);
-    void sortingUrls(const std::string &filePath);
-    std::string getUrl(const std::string &Url);
+  UrlManager(const std::string &mongo_uri = "mongodb://localhost:27017/",
+             const std::string &database_name = "testDb",
+             const std::string &client_name = "admin")
+      : instance_{}, client_{mongocxx::uri{mongo_uri}}, is_connected_{false} {
+    connectToMongoDB(mongo_uri, database_name, client_name);
+  }
+  bool sortingUrls(std::string url);
+  bool sortingUrls(std::vector<std::string> urls);
+  std::vector<Result_read> getUrl(std::vector<std::string> domains);
+  Result_read getUrl(std::string domain);
+  ~UrlManager() {}
 
 private:
-     FileOperations& fileOperator;
+  // from up to down , instance of mongo , then a client , ...
+  mongocxx::instance instance_;
+  mongocxx::client client_;
+  mongocxx::database database_;
+  mongocxx::collection collection_;
+  bool is_connected_ = false;
+  void retryConnection(int interval_seconds);
+  void connectToMongoDB(const std::string &mongo_uri,
+                        const std::string &database_name,
+                        const std::string &client_name) {
+    try {
+      // Access the "admin" database and run the ping command
+      auto admin = client_[client_name];
+      admin.run_command(bsoncxx::from_json(R"({ "ping": 1 })"));
+
+      // Access the specified database
+      database_ = client_[database_name];
+
+      // If no exception is thrown, the connection is successful
+      is_connected_ = true;
+      std::cout << "Successfully connected to MongoDB!" << std::endl;
+    } catch (const mongocxx::exception &e) {
+      std::cerr << "An exception occurred: " << e.what() << std::endl;
+      is_connected_ = false;
+    }
+  }
 };
 
 #endif // URL_MANAGER_H
